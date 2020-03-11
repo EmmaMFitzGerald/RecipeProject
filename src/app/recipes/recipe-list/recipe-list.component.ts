17eb784d437eca717f6a66bce69187db060a2983
Emma FitzGerald <emmamariafitzgerald@gmail.com>
2020-03-11 14:13:13 -0400

    created recipe detail component

--- src/app/recipes/recipe-list/recipe-list.component.ts
+++ src/app/recipes/recipe-list/recipe-list.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-
+import { Recipe } from '../recipe.model'
 @Component({
   selector: 'app-recipe-list',
   templateUrl: './recipe-list.component.html',
@@ -7,7 +7,9 @@ import { Component, OnInit } from '@angular/core';
 })
 export class RecipeListComponent implements OnInit {
 
-  recipes = [];
+  recipes: Recipe[] = [
+    new Recipe("a test recipe", "simply descripton", "https://cooking.nytimes.com/recipes/1014832-pasta-alla-norma")
+  ];
 
   constructor() { }
 
